94657764162b91d6ace3118e326a298533d4ddae
swethamothe <89130824+swethamothe@users.noreply.github.com>
2022-04-03 18:46:47 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+  backend "remote" {
+    organization = "tafzs"
+    workspaces {
+      name = "dev"    
+    }
+  }
+}
+
+
+provider "aws" {
+	region  = var.region
+}
+    
+resource "aws_instance" "main" {
+	ami           = var.ami
+	instance_type = var.instance_type
+}
+
+resource "aws_vpc" "myvpc" {
+    cidr_block = var.vpc
+}
+
+resource "aws_subnet" "subnet" {
+    vpc_id = aws_vpc.myvpc.id
+    cidr_block = var.subnet
+}
